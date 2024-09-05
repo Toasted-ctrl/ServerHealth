9099c4072ff61776508e79cc0a76ae948efe0267
MB-dev-875 <themickbb@gmail.com>
2024-09-05 19:47:39 +0200

    Update: Fixed checkOngoingProcesses, now return "Service running" if service found, and "Service not running" if absent.

--- checkOngoingProcesses.py
+++ checkOngoingProcesses.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import datetime
 from dotenv import dotenv_values, load_dotenv
 
 
@@ -8,8 +9,21 @@ ongoing_process_server_1 = os.getenv("server_1_ongoing_process_001")
 
 def checkOngoingProcesses(processName):
 
-    process_check_command = ["ps aux | grep [m]ain.py | grep -v grep"]
+    process_check_command = [f"""ps aux | grep {ongoing_process_server_1} | grep -v grep"""]
 
-    print(subprocess.check_output(process_check_command))
+    try:
+
+        process_check_call = subprocess.check_output(process_check_command, shell=True)
+
+        print(process_check_call)
+
+        print("Service running")
+
+    except subprocess.CalledProcessError as e:
+
+        print(e.output)
+
+        print("Service not running")
+
 
 checkOngoingProcesses(ongoing_process_server_1)
