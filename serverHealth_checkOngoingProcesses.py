d142715e21bd436d7e3d5130aab58a826ae08835
MB-dev-875 <themickbb@gmail.com>
2024-09-07 12:26:24 +0200

    Created new checkOngoingProcess file.

--- serverHealth_checkOngoingProcesses.py
+++ serverHealth_checkOngoingProcesses.py
@@ -1,29 +1,26 @@
-import os
-import subprocess
-import datetime
-from dotenv import dotenv_values, load_dotenv
 
 
-load_dotenv()
-ongoing_process_server_1 = os.getenv("server_1_ongoing_process_001")
-
-def checkOngoingProcesses(processName):
-
-    process_check_command = [f"""ps aux | grep {ongoing_process_server_1} | grep -v grep"""]
-
-    try:
-
-        process_check_call = subprocess.check_output(process_check_command, shell=True)
-
-        print(process_check_call)
-
-        print("Service running")
-
-    except subprocess.CalledProcessError as e:
-
-        print(e.output)
-
-        print("Service not running")
+from serverHealth_builtComponents import listProcessesToBeChecked, checkOngoingProcesses
 
 
-checkOngoingProcesses(ongoing_process_server_1)
+retrieveprocesses = listProcessesToBeChecked()
+
+if retrieveprocesses[0] == 200:
+
+    listProcesses = retrieveprocesses[1]
+
+    for listProcess in listProcesses:
+
+        processOngoing = checkOngoingProcesses(listProcess)
+
+        if processOngoing == 200:
+
+            print(f"{listProcess} is currently running")
+
+        elif processOngoing == 400:
+
+            print(f"{listProcess} is not running.")
+
+else:
+        
+    print(retrieveprocesses[0])
