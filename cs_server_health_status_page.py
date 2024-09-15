f4b61bd011e37b554239bc24acf9f9a5705576aa
MB-dev-875 <themickbb@gmail.com>
2024-09-15 12:33:20 +0200

    Initiated cs_server_health_status_page to allow users on local network to check ongoing services.

---
+++ cs_server_health_status_page.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pandas as pd
+
+#page title
+st.set_page_config(page_title="This is my first page")
+
+st.title("Server status page")
+
+
+
+
+
+
+st.divider()
+
+st.header("S1: Honey Badger")
+st.text("Placeholder for first server data.")
+
+
+
+
+
+
+
+st.divider()
+
+st.header("S2: Hippopotamus")
+st.text("Placeholder for second server data.")
