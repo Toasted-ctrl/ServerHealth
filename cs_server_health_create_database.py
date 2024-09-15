c03447709fb84a3b652acb2a090e3c28e4380d94
MB-dev-875 <themickbb@gmail.com>
2024-09-15 12:32:21 +0200

    Initiated new scipto, allowing required databases to be created on the check server machine.

---
+++ cs_server_health_create_database.py
@@ -0,0 +1,36 @@
+from sqlalchemy import create_engine, inspect
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.sql import func
+from sqlalchemy import Column, Integer, String, BigInteger, DateTime, Date, Float
+from sqlalchemy.dialects.postgresql import CIDR
+from dotenv import dotenv_values, load_dotenv
+import os
+
+load_dotenv()
+
+db_database = os.getenv("db_database")
+db_password = os.getenv("db_password")
+db_port_id = os.getenv("db_port_id")
+db_hostname = os.getenv("db_hostname")
+db_user = os.getenv("db_user")
+db_method_db = os.getenv("db_method_db")
+db_method_conn = os.getenv("db_method_conn")
+
+engine = create_engine(f"{db_method_db}+{db_method_conn}://{db_user}:{db_password}@{db_hostname}:{db_port_id}/{db_database}")
+
+inspection = inspect(engine)
+
+Base = declarative_base()
+
+table_1_name = "server_identities"
+table_1_check = inspection.has_table(table_1_name)
+
+if table_1_check == False:
+    print(f"'{table_1_name}' = missing, attempting to create table.")
+
+    class list_localhost_ongoing_processes(Base):
+        __tablename__ = table_1_name
+        server_name = Column(String(30), primary_key=True, nullable=False)
+        server_ip_address_IPv4 = Column(CIDR, unique=True)
+
+    Base.metadata.create_all(engine)
