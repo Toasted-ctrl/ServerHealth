85412d0a346052d7b04a9af5d5aedc9759ae7b8a
MB-dev-875 <themickbb@gmail.com>
2024-09-16 20:10:16 +0200

    Updated to display ping timestamp_log and ping_pass_fail of server 1.

--- cs_server_health_status_page.py
+++ cs_server_health_status_page.py
@@ -1,5 +1,11 @@
 import streamlit as st
 import pandas as pd
+import os
+from cs_server_health_builtcomponents_page import display_last_ping_response
+from dotenv import dotenv_values, load_dotenv
+
+#import server ip address
+server_1 = os.getenv("control_server_host_machine_1")
 
 #page title
 st.set_page_config(page_title="This is my first page")
@@ -8,18 +14,14 @@ st.title("Server status page")
 
 
 
-
-
-
 st.divider()
 
 st.header("S1: Honey Badger")
-st.text("Placeholder for first server data.")
 
+server_1_ping_response = display_last_ping_response(server_1)
 
-
-
-
+st.text(f"Last ping: {server_1_ping_response[0]}")
+st.text(f"Ping response: {server_1_ping_response[1]}")
 
 
 st.divider()
