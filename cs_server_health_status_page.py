2983d554e415ad205c66ef858bdfc957c9479e2d
MB-dev-875 <themickbb@gmail.com>
2024-09-18 19:51:05 +0200

    Implemented create_list_active_servers with 60 second cache. Function will output new text block for each server and provide their latest ping status.

--- cs_server_health_status_page.py
+++ cs_server_health_status_page.py
@@ -1,30 +1,50 @@
 import streamlit as st
 import pandas as pd
+import datetime
 import os
-from cs_server_health_builtcomponents_page import display_last_ping_response
+from cs_server_health_builtcomponents_page import display_last_ping_response, retrieve_server_identities
+from cs_server_health_builtcomponents import retrieve_host_machines_ip_addresses
 from dotenv import dotenv_values, load_dotenv
-
-#import server ip address
-server_1 = os.getenv("control_server_host_machine_1")
 
 #page title
 st.set_page_config(page_title="This is my first page")
 
-st.title("Server status page")
+st.title("Server overview")
 
+@st.cache_data(ttl=60)
+def create_list_active_servers():
 
+    server_identity_dataframe = retrieve_server_identities()
 
-st.divider()
+    date_of_retrieving_server_identities = datetime.datetime.now()
+    st.text(f"Date: {date_of_retrieving_server_identities}")
 
-st.header("S1: Honey Badger")
+    if server_identity_dataframe[0] == 400:
 
-server_1_ping_response = display_last_ping_response(server_1)
+        st.text(f"Currently no servers in checking database.")
 
-st.text(f"Last ping: {server_1_ping_response[0]}")
-st.text(f"Ping response: {server_1_ping_response[1]}")
+    if server_identity_dataframe[0] == 200:
 
+        server_dataframe = server_identity_dataframe[2]
 
-st.divider()
+        st.dataframe(server_dataframe)
+        number_of_servers = server_identity_dataframe[1]
 
-st.header("S2: Hippopotamus")
-st.text("Placeholder for second server data.")
+        for i in range(number_of_servers):
+
+            server_name = server_dataframe.iloc[i]['server_name']
+            server_ip_address = server_dataframe.iloc[i]['server_ip_address']
+
+            server_ping_check = display_last_ping_response(server_ip_address)
+            server_last_ping_timestamp = server_ping_check[0]
+            server_last_ping_status = server_ping_check[1]
+
+            st.divider()
+            st.header(f"Server {i}: {server_name}")
+
+            st.text(f"Server IP address: {server_ip_address}")
+
+            st.text(f"Last ping timestamp: {server_last_ping_timestamp}")
+            st.text(f"Last ping status: {server_last_ping_status}")
+
+create_list_active_servers()
