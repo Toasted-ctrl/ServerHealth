52d032ebfdf98961a70a6e6bc1387956c21ed4ca
MB-dev-875 <themickbb@gmail.com>
2024-09-19 12:03:36 +0200

    Implemented retrieve_server_systemload to retrieve system data such as CPU temperature and RAM usage from remote machine.

--- cs_server_health_builtcomponents_page.py
+++ cs_server_health_builtcomponents_page.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import psycopg2
 import os
-
+import datetime
 from sqlalchemy import create_engine
+from datetime import timedelta
 from dotenv import load_dotenv, dotenv_values
 
 load_dotenv()
@@ -14,13 +15,14 @@ db_user = os.getenv("db_user")
 db_method_db = os.getenv("db_method_db")
 db_method_conn = os.getenv("db_method_conn")
 
-engine = create_engine(f"{db_method_db}+{db_method_conn}://{db_user}:{db_password}@{db_hostname}:{db_port_id}/{db_database}")
+engine_local = create_engine(f"{db_method_db}+{db_method_conn}://{db_user}:{db_password}@{db_hostname}:{db_port_id}/{db_database}")
+engine_remote = create_engine(f"{db_method_db}+{db_method_conn}://{db_user}:{db_password}@{db_hostname}:{db_port_id}/{db_database}")
 
 def display_last_ping_response(ip_address):
 
     sql_retrieve_last_ping_status = (f"SELECT timestamp_log, ping_pass_fail FROM check_ping_response WHERE server_ip_address = '{ip_address}' ORDER BY timestamp_log DESC LIMIT 1")
 
-    last_ping_response_df = pd.read_sql(sql=sql_retrieve_last_ping_status, con=engine)
+    last_ping_response_df = pd.read_sql(sql=sql_retrieve_last_ping_status, con=engine_local)
 
     if last_ping_response_df.empty:
 
@@ -38,7 +40,7 @@ def retrieve_server_identities():
 
     sql_retrieve_server_identities = "SELECT * FROM server_identities"
 
-    server_identities_df = pd.read_sql(sql=sql_retrieve_server_identities, con=engine)
+    server_identities_df = pd.read_sql(sql=sql_retrieve_server_identities, con=engine_local)
 
     if server_identities_df.empty:
 
@@ -47,3 +49,22 @@ def retrieve_server_identities():
     elif not server_identities_df.empty:
 
         return(200, server_identities_df.shape[0], server_identities_df)
+    
+def retrieve_server_systemload(remote_hostname, remote_username, remote_password):
+
+    engine_remote = create_engine(f"{db_method_db}+{db_method_conn}://{remote_username}:{remote_password}@{remote_hostname}:{db_port_id}/{db_database}")
+
+    current_date = datetime.datetime.now()
+    prior_date = datetime.datetime.now() + timedelta(days=-1)
+
+    sql_retrieve_server_systemload = (f"SELECT * FROM check_system_load WHERE timestamp_log <= '{current_date}' AND timestamp_log >= '{prior_date}' ORDER BY timestamp_log DESC")
+
+    server_system_load_df = pd.read_sql(sql=sql_retrieve_server_systemload, con=engine_remote)
+
+    if server_system_load_df.empty:
+
+        return([400])
+    
+    elif not server_system_load_df.empty:
+
+        return(200, server_system_load_df.shape[0], server_system_load_df)
