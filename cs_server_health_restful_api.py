d8378608ba4534bb4d0a4bc6bf20d1b787b852cb
MB-dev-875 <themickbb@gmail.com>
2024-09-28 18:51:13 +0200

    Updated remote_server_systemload to use retrieve_api_access_rights function.

--- cs_server_health_restful_api.py
+++ cs_server_health_restful_api.py
@@ -1,7 +1,7 @@
 import json
 
 from flask import Flask, jsonify, request
-from cs_server_health_builtcomponents_page import display_last_ping_response, retrieve_server_identities, retrieve_server_systemload, retrieve_remote_server_credentials
+from cs_server_health_builtcomponents_page import display_last_ping_response, retrieve_server_identities, retrieve_server_systemload, retrieve_remote_server_credentials, retrieve_api_access_rights
 
 app = Flask(__name__)
 
@@ -43,8 +43,6 @@ def remote_server_identities():
 @app.route('/remote_server_systemload', methods=['get'])
 def remote_server_systemload():
 
-    ## NEED TO IMPLEMENT API_KEY TO THIS FUNCTION. BUILD API_KEY check function in cs_server_health_builtcomponents_page
-
     hostname = request.args.get('hostname', None)
     api_key = request.args.get('api_key', None)
 
@@ -54,56 +52,74 @@ def remote_server_systemload():
 
             return jsonify ({'error': 'missing_api_key'})
         
-        elif api_key == 'TEST123':
+        elif not api_key == None:
 
-            retrieve_server_credentials_hostname = (f"{hostname}/32")
+            api_key_access_rights = retrieve_api_access_rights(api_key)
 
-            remote_server_credential_request = retrieve_remote_server_credentials(retrieve_server_credentials_hostname, 'serverhealth_logs')
+            if api_key_access_rights[0] == 200:
 
-            if remote_server_credential_request[0] == 200:
+                if hostname == None:
 
-                remote_server_hostname = remote_server_credential_request[1]
-                remote_server_username = remote_server_credential_request[2]
-                remote_server_password = remote_server_credential_request[3]
-                remote_server_port = remote_server_credential_request[4]
+                    return jsonify ({'error': 'missing_hostname'})
+                    
+                elif not hostname == None:
 
-                result = retrieve_server_systemload(remote_server_hostname, remote_server_username, remote_server_password, 'serverhealth_logs', remote_server_port)
+                    retrieve_server_credentials_hostname = (f"{hostname}/32")
 
-                if result[0] == 400:
+                    remote_server_credential_request = retrieve_remote_server_credentials(retrieve_server_credentials_hostname, 'serverhealth_logs')
 
-                    return jsonify ({'error': 'empty_dataframe'})
+                    if remote_server_credential_request[0] == 200:
+
+                        remote_server_hostname = remote_server_credential_request[1]
+                        remote_server_username = remote_server_credential_request[2]
+                        remote_server_password = remote_server_credential_request[3]
+                        remote_server_port = remote_server_credential_request[4]
+
+                        result = retrieve_server_systemload(remote_server_hostname, remote_server_username, remote_server_password, 'serverhealth_logs', remote_server_port)
+
+                        if result[0] == 400:
+
+                            return jsonify ({'error': 'empty_dataframe_systemload'})
+                        
+                        elif result[0] == 200:
+
+                            number_of_entries = result[1]
+                            cpu_temp_max = result[2]
+                            cpu_temp_mean = result[3]
+                            system_memory_total = int(result[4])
+                            system_memory_available_max = int(result[6])
+                            system_memory_available_mean = int(result[7])
+                            system_memory_free_max = int(result[8])
+                            system_memory_free_mean = int(result[9])
+
+                            return jsonify ({'systemload_records': number_of_entries,
+                                            'systemload_cpu_temp_max': cpu_temp_max,
+                                            'systemload_cup_temp_mean': cpu_temp_mean,
+                                            'systemload_memory_total': system_memory_total,
+                                            'systemload_memory_available_max': system_memory_available_max,
+                                            'systemload_memory_available_mean': system_memory_available_mean,
+                                            'systemload_memory_free_max': system_memory_free_max,
+                                            'systemload_memory_free_mean': system_memory_free_mean})
+                        
+                        elif result[0] == 404:
+
+                            return jsonify ({'error': 'error_on_calling_systemload'})
+                        
+                    elif remote_server_credential_request[0] == 400:
+
+                        return jsonify ({'error': 'empty_dataframe_credentials'})
+
+                    elif remote_server_credential_request[0] == 404:
+
+                        return jsonify ({'error': 'failure_on_obtaining_remote_server_credentials'})
                 
-                elif result[0] == 200:
+            elif api_key_access_rights[0] == 400:
 
-                    number_of_entries = result[1]
-                    cpu_temp_max = result[2]
-                    cpu_temp_mean = result[3]
-                    system_memory_total = int(result[4])
-                    system_memory_available_max = int(result[6])
-                    system_memory_available_mean = int(result[7])
-                    system_memory_free_max = int(result[8])
-                    system_memory_free_mean = int(result[9])
+                return jsonify ({'error': 'nonexisting_api_key'})
+            
+            elif api_key_access_rights[0] == 404:
 
-                    return jsonify ({'systemload_records': number_of_entries,
-                                    'systemload_cpu_temp_max': cpu_temp_max,
-                                    'systemload_cup_temp_mean': cpu_temp_mean,
-                                    'systemload_memory_total': system_memory_total,
-                                    'systemload_memory_available_max': system_memory_available_max,
-                                    'systemload_memory_available_mean': system_memory_available_mean,
-                                    'systemload_memory_free_max': system_memory_free_max,
-                                    'systemload_memory_free_mean': system_memory_free_mean})
-                
-                elif result[0] == 404:
-
-                    return jsonify ({'error': 'error_on_calling_systemload'})
-                
-            elif remote_server_credential_request[0] == 400:
-
-                return jsonify ({'error': 'empty_dataframe'})
-
-            elif remote_server_credential_request[0] == 404:
-
-                return jsonify ({'error': 'failure_on_obtaining_remote_server_credentials'})
+                return jsonify ({'error': 'unexpected_error_on_validating_api_key'})
 
     except:
 
