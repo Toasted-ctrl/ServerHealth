3531678421a99ca97aa1369bbc6fd579cd674fa5
MB-dev-875 <themickbb@gmail.com>
2024-09-29 19:28:05 +0200

    Implemented remote_server_running_processes and remote_server_running_process_status.

--- cs_server_health_restful_api.py
+++ cs_server_health_restful_api.py
@@ -1,7 +1,7 @@
 import json
 
 from flask import Flask, jsonify, request, Response
-from cs_server_health_builtcomponents_page import display_last_ping_response, retrieve_server_identities, retrieve_server_systemload, retrieve_remote_server_credentials, retrieve_api_access_rights
+from cs_server_health_builtcomponents_page import display_last_ping_response, retrieve_server_identities, retrieve_server_systemload, retrieve_remote_server_credentials, retrieve_api_access_rights, retrieve_remote_server_list_continuous_processes, retrieve_remote_server_status_continuous_process
 
 app = Flask(__name__)
 
@@ -177,6 +177,152 @@ def remote_server_systemload():
     except:
 
         return jsonify ({'error': 'unknown_error'})
+    
+@app.route('/remote_server_running_processes', methods=['get'])
+def remote_server_running_processes():
+
+    hostname = request.args.get('hostname', None)
+    api_key = request.args.get('api_key', None)
+
+    try:
+
+        if api_key == None:
+
+            return jsonify ({'error': 'missing_api_key'})
+        
+        elif not api_key == None:
+
+            api_key_access_rights = retrieve_api_access_rights(api_key)
+
+            if api_key_access_rights[0] == 400:
+
+                return jsonify ({'error': 'invalid_api_key'})
+            
+            elif api_key_access_rights[0] == 404:
+
+                return jsonify ({'error': 'unexpected_error_on_api_key_validation'})
+            
+            elif api_key_access_rights[0] == 200:
+
+                if hostname == None:
+
+                    return jsonify ({'error': 'missing_hostname'})
+                
+                elif not hostname == None:
+
+                    remote_server_credentials = retrieve_remote_server_credentials(hostname, 'serverhealth_logs')
+
+                    if remote_server_credentials[0] == 400:
+
+                        return jsonify ({'error': 'empty_dataframe_credentials'})
+                    
+                    elif remote_server_credentials[0] == 404:
+
+                        return jsonify ({'error': 'unexpected_error_on_remote_server_credential_retrieval'})
+                    
+                    elif remote_server_credentials[0] == 200:
+
+                        remote_server_username = remote_server_credentials[2]
+                        remote_server_password = remote_server_credentials[3]
+                        remote_server_port = remote_server_credentials[4]
+
+                        remote_server_running_processes_list = retrieve_remote_server_list_continuous_processes(hostname, remote_server_username, remote_server_password, 'serverhealth_logs', remote_server_port)
+
+                        if remote_server_running_processes_list[0] == 400:
+
+                            return jsonify ({'error': 'empty_dataframe_running_processes'})
+                        
+                        elif remote_server_running_processes_list[0] == 404:
+
+                            return jsonify ({'error': 'unexpected_error_process_retrieval'})
+                        
+                        elif remote_server_running_processes_list[0] == 200:
+
+                            list_processes_dataframe = remote_server_running_processes_list[3]
+
+                            return Response (list_processes_dataframe.to_json(orient="records"), mimetype='application/json')
+
+    except:
+
+        return jsonify ({'error': 'unknown_error'})
+    
+@app.route('/remote_server_running_process_status', methods=['get'])
+def remote_server_running_process_status():
+
+    hostname = request.args.get('hostname', None)
+    api_key = request.args.get('api_key', None)
+    process_name = request.args.get('process_name', None)
+
+    try:
+
+        if api_key == None:
+
+            return jsonify ({'error': 'missing_api_key'})
+        
+        elif not api_key == None:
+
+            api_key_access_rights = retrieve_api_access_rights(api_key)
+
+            if api_key_access_rights[0] == 400:
+
+                return jsonify ({'error': 'invalid_api_key'})
+            
+            elif api_key_access_rights[0] == 404:
+
+                return jsonify ({'error': 'error_on_validiting_api_key'})
+            
+            elif api_key_access_rights[0] == 200:
+
+                if process_name == None:
+
+                    return jsonify ({'error': 'missing_process_name'})
+                
+                elif not process_name == None:
+
+                    if hostname == None:
+
+                        return jsonify ({'error': 'missing_hostname'})
+                    
+                    elif not hostname == None:
+
+                        remote_server_credentials = retrieve_remote_server_credentials(hostname, 'serverhealth_logs')
+
+                        if remote_server_credentials[0] == 400:
+
+                            return jsonify ({'error': 'empty_dataframe_credentials'})
+                    
+                        elif remote_server_credentials[0] == 404:
+
+                            return jsonify ({'error': 'unexpected_error_on_remote_server_credential_retrieval'})
+                    
+                        elif remote_server_credentials[0] == 200:
+
+                            remote_server_username = remote_server_credentials[2]
+                            remote_server_password = remote_server_credentials[3]
+                            remote_server_port = remote_server_credentials[4]
+
+                            remote_server_continuous_process_status = retrieve_remote_server_status_continuous_process(hostname, remote_server_username, remote_server_password, 'serverhealth_logs', remote_server_port, process_name)
+
+                            if remote_server_continuous_process_status[0] == 400:
+
+                                return jsonify ({'error': 'empty_dataframe_processes'})
+                            
+                            elif remote_server_continuous_process_status[0] == 404:
+
+                                return jsonify ({'error': 'could_not_retrieve_process_status'})
+                            
+                            elif remote_server_continuous_process_status[0] == 200:
+
+                                number_of_entries = remote_server_continuous_process_status[1]
+                                number_of_fails = remote_server_continuous_process_status[2]
+                                last_entry_timestamp = remote_server_continuous_process_status[3]
+
+                                return jsonify ({'24h_measurements': number_of_entries, '24h_fails': number_of_fails, 'last_measurement': last_entry_timestamp})
+
+    except:
+
+        return jsonify ({'error': 'unknown_error'})    
+
 
 if __name__ == '__main__':
     app.run()
