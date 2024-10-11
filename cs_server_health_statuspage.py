9fbaa270ae572f41a5b34302579ccbe37dfc808f
MB-dev-875 <themickbb@gmail.com>
2024-10-11 19:46:55 +0200

    Replaced all print statements with streamlit text, headers, subheaders and dividers. Fixed an issue where data reloads would only show completed but not failed reloads. Set page config and page title. Introduced fix to show what error was returned by API call instead of only showing an error was returned.

--- cs_server_health_statuspage.py
+++ cs_server_health_statuspage.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python3
+
 import streamlit as st
-import datetime
 import os
 import requests
+import datetime
 from dotenv import load_dotenv
 
 #load api key and main api link
@@ -21,6 +23,13 @@ extension_remote_server_scheduled_reloads_status = "/remote_server_scheduled_rel
 #error keys to check for in JSON objects
 check_error_key = "error"
 
+st.set_page_config(page_title="Server status")
+
+st.title("Server overview")
+
+date_of_retrieving_server_identities = datetime.datetime.now()
+st.text(f"Date: {date_of_retrieving_server_identities}")
+
 def create_list_active_servers():
 
     request_remote_server_identities_url = (f"{api_link}{extension_remote_server_identities}")
@@ -30,16 +39,13 @@ def create_list_active_servers():
 
     if request_remote_server_identities_response.status_code == 404:
 
-        print ("error on calling api")
+        st.text("error on calling api")
 
     elif request_remote_server_identities_response.status_code == 200:
 
-        print ("Success")
-
         if check_error_key in request_remote_server_identities_response.json():
 
-            print ("error in json response")
-            print (f"error code: {request_remote_server_identities_response.json()['error']}")
+            st.text(f"error code: {request_remote_server_identities_response.json()['error']}")
 
         elif not check_error_key in request_remote_server_identities_response.json():
 
@@ -51,20 +57,18 @@ def create_list_active_servers():
                 remote_server_name = request_remote_server_identities_response.json()[i]['server_name']
                 remote_server_ip_address = request_remote_server_identities_response.json()[i]['server_ip_address'].replace("/32", "")
 
-                print(f"Server {i}: {remote_server_name} / {remote_server_ip_address}")
+                st.text(f"Server {i}: {remote_server_name} / {remote_server_ip_address}")
  
             #for each server, create report
             for i in range (number_of_remote_servers):
 
-                #TODO: insert streamlit divider here
-
-                
+                st.divider()
 
                 remote_server_name = request_remote_server_identities_response.json()[i]['server_name']
                 remote_server_ip_address = request_remote_server_identities_response.json()[i]['server_ip_address'].replace("/32", "")
 
-                print(f"Server {i}: {remote_server_name}") #TODO: convert to streamlit header
-                print(f"Server IP address: {remote_server_ip_address}")
+                st.header(f"Server {i}: {remote_server_name}")
+                st.text(f"Server IP address: {remote_server_ip_address}")
 
                 #server last ping section
 
@@ -73,22 +77,22 @@ def create_list_active_servers():
 
                 request_remote_server_last_ping_response = requests.get(request_remote_server_last_ping_url, params=request_remote_server_last_ping_params)
 
-                print(f"Server's last ping response")
+                st.text(f"Server's last ping response:")
 
                 if request_remote_server_last_ping_response.status_code == 404:
 
-                    print(f"Error: API call error")
+                    st.text(f"Error: API call error")
 
                 elif request_remote_server_last_ping_response.status_code == 200:
 
-                    print(f"""
+                    st.text(f"""
                           >> Ping timestamp: {request_remote_server_last_ping_response.json()['ping_timestamp_log']}
                           >> Ping status: {request_remote_server_last_ping_response.json()['ping_status']}
                           """)
                 
                 #server systemload section
 
-                print(f"Server systemload during last 24 hours") #TODO: replace with streamlit subheader
+                st.subheader(f"Server systemload during last 24 hours")
 
                 request_remote_server_systemload_url = (f"{api_link}{extension_remote_server_systemload}")
                 request_remote_server_systemload_params = {'api_key': api_key, 'hostname': remote_server_ip_address}
@@ -97,20 +101,20 @@ def create_list_active_servers():
 
                 if request_remote_server_systemload_response.status_code == 404:
 
-                    print(f"Error: API call error")
+                    st.text(f"Error: API call error")
 
                 elif request_remote_server_systemload_response.status_code == 200:
 
                     if check_error_key in request_remote_server_systemload_response.json():
 
-                        print(f"Error: could not retrieve systemload data.")
+                        st.text(f"Error: {request_remote_server_systemload_response.json()['error']}")
 
                     elif not check_error_key in request_remote_server_systemload_response.json():
 
-                        print(f"Number of entries: {request_remote_server_systemload_response.json()['systemload_records']}")
-                        print(f"Avg CPU temperature: {request_remote_server_systemload_response.json()['systemload_cup_temp_mean']} (Max: {request_remote_server_systemload_response.json()['systemload_cpu_temp_max']})")
-                        print(f"System memory:")
-                        print(f"""
+                        st.text(f"Number of entries: {request_remote_server_systemload_response.json()['systemload_records']}")
+                        st.text(f"Avg CPU temperature: {request_remote_server_systemload_response.json()['systemload_cup_temp_mean']} (Max: {request_remote_server_systemload_response.json()['systemload_cpu_temp_max']})")
+                        st.text(f"System memory:")
+                        st.text(f"""
                               >> Total - {request_remote_server_systemload_response.json()['systemload_memory_total']}
                               >> Available - Avg: {request_remote_server_systemload_response.json()['systemload_memory_available_mean']}, Max: {request_remote_server_systemload_response.json()['systemload_memory_available_max']}
                               >> Free - Avg: {request_remote_server_systemload_response.json()['systemload_memory_free_mean']}, Max: {request_remote_server_systemload_response.json()['systemload_memory_free_max']}
@@ -118,7 +122,7 @@ def create_list_active_servers():
                         
                 # running services section
 
-                print(f"Running services") #TODO: Replace with streamlist subheader
+                st.subheader(f"Running services")
 
                 request_remote_server_running_services_url = (f"{api_link}{extension_remote_server_running_services}")
                 request_remote_server_running_services_params = {'api_key': api_key, 'hostname': remote_server_ip_address}
@@ -127,13 +131,13 @@ def create_list_active_servers():
 
                 if request_remote_server_running_services_response.status_code == 404:
 
-                    print(f"Error: API call error")
+                    st.text(f"Error: API call error")
 
                 elif request_remote_server_running_services_response.status_code == 200:
 
                     if check_error_key in request_remote_server_running_services_response.json():
 
-                        print(f"Error: Could not retrieve list of running services")
+                        st.text(f"Error: {request_remote_server_running_services_response.json()['error']}")
 
                     elif not check_error_key in request_remote_server_running_services_response.json():
 
@@ -143,7 +147,7 @@ def create_list_active_servers():
 
                             running_service_name = request_remote_server_running_services_response.json()[i]['process_name']
 
-                            print(f"Process name: {running_service_name}")
+                            st.text(f"Process name: {running_service_name}")
 
                             #running services status section
 
@@ -154,17 +158,17 @@ def create_list_active_servers():
 
                             if request_remote_server_running_services_status_response.status_code == 404:
 
-                                print(f"Error: API call error")
+                                st.text(f"Error: API call error")
 
                             elif request_remote_server_running_services_status_response.status_code == 200:
 
                                 if check_error_key in request_remote_server_running_services_status_response.json():
 
-                                    print(f"Error: Could not retrieve latest status")
+                                    st.text(f"Error: {request_remote_server_running_services_status_response.json()['error']}")
 
                                 elif not check_error_key in request_remote_server_running_services_status_response.json():
 
-                                    print(f"""
+                                    st.text(f"""
                                           >> Last reload status: {request_remote_server_running_services_status_response.json()['last_measurement']}
                                           >> 24h checks: {request_remote_server_running_services_status_response.json()['24h_measurements']}
                                           >> 24h fails: {request_remote_server_running_services_status_response.json()['24h_fails']}
@@ -172,7 +176,7 @@ def create_list_active_servers():
                                     
                 # API call/data loads section
 
-                print(f"API calls/data loads") #TODO: Replace with streamlit subheader
+                st.subheader(f"API calls/data loads")
 
                 request_remote_server_data_reloads_url = (f"{api_link}{extension_remote_server_scheduled_reloads}")
                 request_remote_server_data_reloads_params = {'api_key': api_key, 'hostname': remote_server_ip_address}
@@ -181,13 +185,13 @@ def create_list_active_servers():
 
                 if request_remote_server_data_reloads_response.status_code == 404:
 
-                    print(f"Error: API call error")
+                    st.text(f"Error: API call error")
 
                 elif request_remote_server_data_reloads_response.status_code == 200:
 
                     if check_error_key in request_remote_server_data_reloads_response.json():
 
-                        print(f"Error: Could not retrieve list of API calls/data loads")
+                        st.text(f"Error: {request_remote_server_data_reloads_response.json()['error']}")
 
                     elif not check_error_key in request_remote_server_data_reloads_response.json():
 
@@ -201,7 +205,7 @@ def create_list_active_servers():
                             reload_table_name = request_remote_server_data_reloads_response.json()[i]['table_name']
                             reload_load_frequency_hours = request_remote_server_data_reloads_response.json()[i]['load_frequency_hours']
 
-                            print(f"DB name: {reload_database_name}, Table name: {reload_table_name}")
+                            st.text(f"DB name: {reload_database_name}, Table name: {reload_table_name}")
 
                             request_remote_server_data_reloads_status_url = (f"{api_link}{extension_remote_server_scheduled_reloads_status}")
                             request_remote_server_data_reloads_status_params = {'api_key': api_key, 'hostname': remote_server_ip_address, 'remote_database_name': reload_database_name, 'reload_name': reload_table_name, 'reload_frequency': reload_load_frequency_hours}
@@ -210,19 +214,26 @@ def create_list_active_servers():
 
                             if request_remote_server_data_reloads_status_response.status_code == 404:
 
-                                print(f"Error: API call error")
+                                st.text(f"Error: API call error")
 
                             elif request_remote_server_data_reloads_status_response.status_code == 200:
 
                                 if check_error_key in request_remote_server_data_reloads_status_response.json():
 
-                                    print(f"Error: Could not retrieve API call/reload status")
+                                    if request_remote_server_data_reloads_status_response.json()['error'] == "empty_dataframe_scheduled_reloads":
 
-                                    #TODO: build something to also show failed data reload.
+                                        st.text(f"""
+                                                >> Last reload status: FAILED
+                                                >> Reloads every {reload_load_frequency_hours}.
+                                                """)
+                                        
+                                    else:
+
+                                        st.text(f"Error: {request_remote_server_data_reloads_status_response.json()['error']}")
 
                                 elif not check_error_key in request_remote_server_data_reloads_status_response.json():
 
-                                    print(f"""
+                                    st.text(f"""
                                     >> Last reload status: COMPLETED
                                     >> Reloads every {reload_load_frequency_hours}. Last reload: {request_remote_server_data_reloads_status_response.json()['last_reload_timestamp']}
                                     >> Number of entries: {request_remote_server_data_reloads_status_response.json()['rows_added']}
